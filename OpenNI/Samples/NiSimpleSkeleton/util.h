efb24f771b36b0a4879a03f59c5afa9bbe23a4f2
Spencer Lee <lesp@seas.upenn.edu>
2012-04-18 03:04:05 -0400

    Removed problematic extra files from repository

--- OpenNI/Samples/NiSimpleSkeleton/util.h
+++
@@ -1,9 +0,0 @@
-#ifndef UTIL_H
-#define UTIL_H
-
-float findAngle(XnSkeletonJointTransformation a,
-                XnSkeletonJointTransformation b,
-                XnSkeletonJointTransformation c,
-                int d);
-
-#endif
