5da14e4d8167e2ea51ebc4425ad599e6fc7af621
Spencer Lee <lesp@seas.upenn.edu>
2012-04-26 19:58:41 -0400

    Added NITE

---
+++ NITE-Bin-Dev-Linux-x86-v1.5.2.21/Samples/CircleControl/x86-Release/main.d
@@ -0,0 +1,358 @@
+x86-Release/main.d x86-Release/main.o: main.cpp \
+ /usr/include/ni/XnOpenNI.h /usr/include/ni/XnTypes.h \
+ /usr/include/ni/XnStatus.h /usr/include/ni/XnPlatform.h \
+ /usr/include/ni/Linux-x86/XnPlatformLinux-x86.h /usr/include/unistd.h \
+ /usr/include/features.h /usr/include/bits/predefs.h \
+ /usr/include/sys/cdefs.h /usr/include/bits/wordsize.h \
+ /usr/include/gnu/stubs.h /usr/include/gnu/stubs-32.h \
+ /usr/include/bits/posix_opt.h /usr/include/bits/environments.h \
+ /usr/include/bits/types.h /usr/include/bits/typesizes.h \
+ /usr/lib/gcc/i686-linux-gnu/4.4.5/include/stddef.h \
+ /usr/include/bits/confname.h /usr/include/getopt.h \
+ /usr/include/bits/unistd.h /usr/include/stdlib.h \
+ /usr/include/bits/waitflags.h /usr/include/bits/waitstatus.h \
+ /usr/include/endian.h /usr/include/bits/endian.h \
+ /usr/include/bits/byteswap.h /usr/include/xlocale.h \
+ /usr/include/sys/types.h /usr/include/time.h /usr/include/sys/select.h \
+ /usr/include/bits/select.h /usr/include/bits/sigset.h \
+ /usr/include/bits/time.h /usr/include/sys/sysmacros.h \
+ /usr/include/bits/pthreadtypes.h /usr/include/alloca.h \
+ /usr/include/bits/stdlib.h /usr/include/stdio.h /usr/include/libio.h \
+ /usr/include/_G_config.h /usr/include/wchar.h \
+ /usr/lib/gcc/i686-linux-gnu/4.4.5/include/stdarg.h \
+ /usr/include/bits/stdio_lim.h /usr/include/bits/sys_errlist.h \
+ /usr/include/bits/stdio.h /usr/include/bits/stdio2.h \
+ /usr/include/string.h /usr/include/bits/string3.h /usr/include/assert.h \
+ /usr/lib/gcc/i686-linux-gnu/4.4.5/include-fixed/limits.h \
+ /usr/lib/gcc/i686-linux-gnu/4.4.5/include-fixed/syslimits.h \
+ /usr/include/limits.h /usr/include/bits/posix1_lim.h \
+ /usr/include/bits/local_lim.h /usr/include/linux/limits.h \
+ /usr/include/bits/posix2_lim.h /usr/include/bits/xopen_lim.h \
+ /usr/include/ni/XnOS.h /usr/include/ni/XnMacros.h \
+ /usr/include/ni/XnStatusCodes.h /usr/include/ni/XnStatus.h \
+ /usr/include/ni/XnOSStrings.h /usr/include/ni/XnOSMemory.h \
+ /usr/include/ni/Linux-x86/XnOSLinux-x86.h /usr/include/pthread.h \
+ /usr/include/sched.h /usr/include/bits/sched.h \
+ /usr/include/bits/setjmp.h /usr/include/sys/stat.h \
+ /usr/include/bits/stat.h /usr/include/fcntl.h /usr/include/bits/fcntl.h \
+ /usr/include/bits/uio.h /usr/include/bits/fcntl2.h \
+ /usr/include/ni/XnContext.h /usr/include/ni/XnTypes.h \
+ /usr/include/ni/XnQueries.h /usr/include/ni/XnPrdNodeInfoList.h \
+ /usr/include/ni/XnLicensing.h /usr/include/ni/XnUtils.h \
+ /usr/include/ni/XnModuleInterface.h /usr/include/ni/XnPrdNodeInfo.h \
+ /usr/include/ni/XnQueries.h /usr/include/ni/XnPrdNode.h \
+ /usr/include/ni/XnEnumerationErrors.h /usr/include/ni/XnVersion.h \
+ ../../Include/XnVHandPointContext.h ../../Include/XnVNiteDefs.h \
+ ../../Include/XnVDirection.h ../../Include/XnVNiteStatus.h \
+ ../../Include/XnCommon.h ../../Include/XnVNiteVersion.h \
+ ../../Include/XnVSessionManager.h ../../Include/XnVDeviceControl.h \
+ /usr/include/ni/XnCppWrapper.h /usr/include/ni/XnCodecIDs.h \
+ ../../Include/XnVMessageListener.h /usr/include/ni/XnThreadSafeQueue.h \
+ /usr/include/ni/XnQueue.h /usr/include/ni/XnList.h \
+ /usr/include/ni/XnDataTypes.h /usr/include/ni/XnOS.h \
+ /usr/include/ni/IXnNodeAllocator.h /usr/include/ni/XnNodeAllocator.h \
+ /usr/include/ni/XnNode.h /usr/include/ni/XnBaseNode.h \
+ /usr/include/ni/XnStatusCodes.h ../../Include/XnVMessage.h \
+ ../../Include/XnVMessageGenerator.h ../../Include/XnVHandle.h \
+ ../../Include/XnVMultipleHands.h ../../Include/XnVHandPointContext.h \
+ ../../Include/XnVGesture.h ../../Include/XnVPointTracker.h \
+ ../../Include/XnVDeviceFilter.h ../../Include/XnVPointMessage.h \
+ ../../Include/XnVSessionGenerator.h ../../Include/XnVSessionListener.h \
+ ../../Include/XnV3DVector.h /usr/include/math.h \
+ /usr/include/bits/huge_val.h /usr/include/bits/huge_valf.h \
+ /usr/include/bits/huge_vall.h /usr/include/bits/inf.h \
+ /usr/include/bits/nan.h /usr/include/bits/mathdef.h \
+ /usr/include/bits/mathcalls.h /usr/include/bits/mathinline.h \
+ ../../Include/XnVMathCommon.h ../../Include/XnVSteadyDetector.h \
+ ../../Include/XnVPointControl.h ../../Include/XnVCircleDetector.h \
+ ../../Include/XnVCircle.h /usr/include/GL/glut.h \
+ /usr/include/GL/freeglut_std.h /usr/include/GL/gl.h \
+ /usr/include/GL/glext.h /usr/include/inttypes.h /usr/include/stdint.h \
+ /usr/include/bits/wchar.h /usr/include/GL/glu.h signal_catch.h \
+ /usr/include/ni/XnPlatform.h
+
+/usr/include/ni/XnOpenNI.h:
+
+/usr/include/ni/XnTypes.h:
+
+/usr/include/ni/XnStatus.h:
+
+/usr/include/ni/XnPlatform.h:
+
+/usr/include/ni/Linux-x86/XnPlatformLinux-x86.h:
+
+/usr/include/unistd.h:
+
+/usr/include/features.h:
+
+/usr/include/bits/predefs.h:
+
+/usr/include/sys/cdefs.h:
+
+/usr/include/bits/wordsize.h:
+
+/usr/include/gnu/stubs.h:
+
+/usr/include/gnu/stubs-32.h:
+
+/usr/include/bits/posix_opt.h:
+
+/usr/include/bits/environments.h:
+
+/usr/include/bits/types.h:
+
+/usr/include/bits/typesizes.h:
+
+/usr/lib/gcc/i686-linux-gnu/4.4.5/include/stddef.h:
+
+/usr/include/bits/confname.h:
+
+/usr/include/getopt.h:
+
+/usr/include/bits/unistd.h:
+
+/usr/include/stdlib.h:
+
+/usr/include/bits/waitflags.h:
+
+/usr/include/bits/waitstatus.h:
+
+/usr/include/endian.h:
+
+/usr/include/bits/endian.h:
+
+/usr/include/bits/byteswap.h:
+
+/usr/include/xlocale.h:
+
+/usr/include/sys/types.h:
+
+/usr/include/time.h:
+
+/usr/include/sys/select.h:
+
+/usr/include/bits/select.h:
+
+/usr/include/bits/sigset.h:
+
+/usr/include/bits/time.h:
+
+/usr/include/sys/sysmacros.h:
+
+/usr/include/bits/pthreadtypes.h:
+
+/usr/include/alloca.h:
+
+/usr/include/bits/stdlib.h:
+
+/usr/include/stdio.h:
+
+/usr/include/libio.h:
+
+/usr/include/_G_config.h:
+
+/usr/include/wchar.h:
+
+/usr/lib/gcc/i686-linux-gnu/4.4.5/include/stdarg.h:
+
+/usr/include/bits/stdio_lim.h:
+
+/usr/include/bits/sys_errlist.h:
+
+/usr/include/bits/stdio.h:
+
+/usr/include/bits/stdio2.h:
+
+/usr/include/string.h:
+
+/usr/include/bits/string3.h:
+
+/usr/include/assert.h:
+
+/usr/lib/gcc/i686-linux-gnu/4.4.5/include-fixed/limits.h:
+
+/usr/lib/gcc/i686-linux-gnu/4.4.5/include-fixed/syslimits.h:
+
+/usr/include/limits.h:
+
+/usr/include/bits/posix1_lim.h:
+
+/usr/include/bits/local_lim.h:
+
+/usr/include/linux/limits.h:
+
+/usr/include/bits/posix2_lim.h:
+
+/usr/include/bits/xopen_lim.h:
+
+/usr/include/ni/XnOS.h:
+
+/usr/include/ni/XnMacros.h:
+
+/usr/include/ni/XnStatusCodes.h:
+
+/usr/include/ni/XnStatus.h:
+
+/usr/include/ni/XnOSStrings.h:
+
+/usr/include/ni/XnOSMemory.h:
+
+/usr/include/ni/Linux-x86/XnOSLinux-x86.h:
+
+/usr/include/pthread.h:
+
+/usr/include/sched.h:
+
+/usr/include/bits/sched.h:
+
+/usr/include/bits/setjmp.h:
+
+/usr/include/sys/stat.h:
+
+/usr/include/bits/stat.h:
+
+/usr/include/fcntl.h:
+
+/usr/include/bits/fcntl.h:
+
+/usr/include/bits/uio.h:
+
+/usr/include/bits/fcntl2.h:
+
+/usr/include/ni/XnContext.h:
+
+/usr/include/ni/XnTypes.h:
+
+/usr/include/ni/XnQueries.h:
+
+/usr/include/ni/XnPrdNodeInfoList.h:
+
+/usr/include/ni/XnLicensing.h:
+
+/usr/include/ni/XnUtils.h:
+
+/usr/include/ni/XnModuleInterface.h:
+
+/usr/include/ni/XnPrdNodeInfo.h:
+
+/usr/include/ni/XnQueries.h:
+
+/usr/include/ni/XnPrdNode.h:
+
+/usr/include/ni/XnEnumerationErrors.h:
+
+/usr/include/ni/XnVersion.h:
+
+../../Include/XnVHandPointContext.h:
+
+../../Include/XnVNiteDefs.h:
+
+../../Include/XnVDirection.h:
+
+../../Include/XnVNiteStatus.h:
+
+../../Include/XnCommon.h:
+
+../../Include/XnVNiteVersion.h:
+
+../../Include/XnVSessionManager.h:
+
+../../Include/XnVDeviceControl.h:
+
+/usr/include/ni/XnCppWrapper.h:
+
+/usr/include/ni/XnCodecIDs.h:
+
+../../Include/XnVMessageListener.h:
+
+/usr/include/ni/XnThreadSafeQueue.h:
+
+/usr/include/ni/XnQueue.h:
+
+/usr/include/ni/XnList.h:
+
+/usr/include/ni/XnDataTypes.h:
+
+/usr/include/ni/XnOS.h:
+
+/usr/include/ni/IXnNodeAllocator.h:
+
+/usr/include/ni/XnNodeAllocator.h:
+
+/usr/include/ni/XnNode.h:
+
+/usr/include/ni/XnBaseNode.h:
+
+/usr/include/ni/XnStatusCodes.h:
+
+../../Include/XnVMessage.h:
+
+../../Include/XnVMessageGenerator.h:
+
+../../Include/XnVHandle.h:
+
+../../Include/XnVMultipleHands.h:
+
+../../Include/XnVHandPointContext.h:
+
+../../Include/XnVGesture.h:
+
+../../Include/XnVPointTracker.h:
+
+../../Include/XnVDeviceFilter.h:
+
+../../Include/XnVPointMessage.h:
+
+../../Include/XnVSessionGenerator.h:
+
+../../Include/XnVSessionListener.h:
+
+../../Include/XnV3DVector.h:
+
+/usr/include/math.h:
+
+/usr/include/bits/huge_val.h:
+
+/usr/include/bits/huge_valf.h:
+
+/usr/include/bits/huge_vall.h:
+
+/usr/include/bits/inf.h:
+
+/usr/include/bits/nan.h:
+
+/usr/include/bits/mathdef.h:
+
+/usr/include/bits/mathcalls.h:
+
+/usr/include/bits/mathinline.h:
+
+../../Include/XnVMathCommon.h:
+
+../../Include/XnVSteadyDetector.h:
+
+../../Include/XnVPointControl.h:
+
+../../Include/XnVCircleDetector.h:
+
+../../Include/XnVCircle.h:
+
+/usr/include/GL/glut.h:
+
+/usr/include/GL/freeglut_std.h:
+
+/usr/include/GL/gl.h:
+
+/usr/include/GL/glext.h:
+
+/usr/include/inttypes.h:
+
+/usr/include/stdint.h:
+
+/usr/include/bits/wchar.h:
+
+/usr/include/GL/glu.h:
+
+signal_catch.h:
+
+/usr/include/ni/XnPlatform.h:
